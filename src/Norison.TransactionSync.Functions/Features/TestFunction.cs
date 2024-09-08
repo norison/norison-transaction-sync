e97aff0d5426466a622a98d68a3175e3feee379e
Ihor Matiev <48079411+norison@users.noreply.github.com>
2024-09-08 18:56:17 +0300

    Added test function (#4)

---
+++ src/Norison.TransactionSync.Functions/Features/TestFunction.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Norison.TransactionSync.Functions.Features;
+
+public class TestFunction(ILogger<TestFunction> logger)
+{
+    [Function(nameof(TestFunction))]
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "test")] HttpRequest req)
+    {
+        await Task.CompletedTask;
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
