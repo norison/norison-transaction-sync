02ab348da839461a6f5d23fd1677a3f41283ab83
Ihor Matiev <ihor.matiev@gmail.com>
2024-09-10 21:54:31 +0300

    Removed dependencies and statement function

--- src/Norison.BankNotionConnector.Functions/Features/AddStatement/AddStatementData.cs
+++
@@ -1,9 +0,0 @@
-using Norison.MonoSdk.Models;
-
-namespace Norison.BankNotionConnector.Functions.Features.AddStatement;
-
-public class AddStatementData
-{
-    public string Account { get; set; } = string.Empty;
-    public Statement StatementItem { get; set; } = new();
-}
