091f6616be44f7abf87329ff05981185df2b0e05
Ihor Matiev <48079411+norison@users.noreply.github.com>
2024-09-16 18:09:35 +0300

    Revert app insights (#18)
    * Revert app insights
    
    * Reading stream before background task
    
    * Refactored

--- src/Norison.TransactionSync.Functions/Features/MonobankWebHookFunction.cs
+++ src/Norison.TransactionSync.Functions/Features/MonobankWebHookFunction.cs
@@ -16,7 +16,7 @@ namespace Norison.TransactionSync.Functions.Features;
 public class MonobankWebHookFunction(ISender sender, IMemoryCache memoryCache)
 {
     [Function(nameof(MonobankWebHookFunction))]
-    public IActionResult RunAsync(
+    public async Task<IActionResult> RunAsync(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "monobank/{chatId}")]
         HttpRequest req)
     {
@@ -25,27 +25,30 @@ public class MonobankWebHookFunction(ISender sender, IMemoryCache memoryCache)
             return new OkResult();
         }
 
-        Task.Run(async () =>
-        {
-            using var reader = new StreamReader(req.Body);
-            var content = await reader.ReadToEndAsync();
-            var webHookModel = JsonSerializer.Deserialize<WebHookModel>(content);
+        using var reader = new StreamReader(req.Body);
+        var content = await reader.ReadToEndAsync();
+        var chatId = long.Parse(req.RouteValues["chatId"]!.ToString()!);
 
-            if (webHookModel is null || memoryCache.TryGetValue(webHookModel.Data.StatementItem.Id, out _))
-            {
-                return;
-            }
-
-            var chatId = long.Parse(req.RouteValues["chatId"]!.ToString()!);
-            var data = webHookModel.Data;
-            var transactionId = data.StatementItem.Id;
-
-            memoryCache.Set(transactionId, transactionId, TimeSpan.FromMinutes(10));
-
-            var command = new ProcessMonoWebHookDataCommand { ChatId = chatId, WebHookData = data };
-            await sender.Send(command);
-        });
+        _ = ProcessWebHookData(chatId, content);
 
         return new OkResult();
+    }
+
+    private async Task ProcessWebHookData(long chatId, string content)
+    {
+        var webHookModel = JsonSerializer.Deserialize<WebHookModel>(content);
+
+        if (webHookModel is null || memoryCache.TryGetValue(webHookModel.Data.StatementItem.Id, out _))
+        {
+            return;
+        }
+
+        var data = webHookModel.Data;
+        var transactionId = data.StatementItem.Id;
+
+        memoryCache.Set(transactionId, transactionId, TimeSpan.FromMinutes(10));
+
+        var command = new ProcessMonoWebHookDataCommand { ChatId = chatId, WebHookData = data };
+        await sender.Send(command);
     }
 }
